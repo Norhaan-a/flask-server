3f2f28f72b23cad54a576551bb09ffdd7eb63c1c
Norhaan-a <norhaan.ahmad1@gmail.com>
2022-01-14 17:08:01 +0100

    added text to index

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_folder="static", static_url_path="")
 
 @app.get('/')
 def index():
-  return render_template('index.html')
+  return render_template('index.html', text='Some random welcome text')
 
 @app.get('/about')
 def about():
